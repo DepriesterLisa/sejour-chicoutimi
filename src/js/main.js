204d7a9996c00f6ee44009e18e10ceeafac213ba
Léo Goemaere <goemaere.leo@hotmail.fr>
2018-05-21 15:42:52 +0200

    Start home styles

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,8 @@ window.addEventListener('DOMContentLoaded', function() {
 
 	var mySwiper = new Swiper ('.swiper-container', {
 		// Optional parameters
-		loop: true
+		loop: true,
+		slidesPerView: 3
     })
 
 });
