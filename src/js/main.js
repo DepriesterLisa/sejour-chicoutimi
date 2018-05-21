88588da41a7fdf9cbc7850d78ea766557fd06017
Léo Goemaere <goemaere.leo@hotmail.fr>
2018-05-21 16:34:37 +0200

    Slider OK

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,11 @@ window.addEventListener('DOMContentLoaded', function() {
 	var mySwiper = new Swiper ('.swiper-container', {
 		// Optional parameters
 		loop: true,
-		slidesPerView: 3
+		slidesPerView: 3,
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		}
     })
 
 });
