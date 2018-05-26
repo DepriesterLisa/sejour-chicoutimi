cf8cabc37eab4257b50d8f65942ee5ce77a5d5e4
Léo Goemaere <goemaere.leo@hotmail.fr>
2018-05-26 14:37:01 +0200

    Style the page list

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 window.addEventListener('DOMContentLoaded', function() {
 
-	var mySwiper = new Swiper ('.swiper-container', {
+	var mySwiper = new Swiper ('.main-slider', {
 		// Optional parameters
 		loop: true,
 		slidesPerView: 3,
@@ -8,7 +8,17 @@ window.addEventListener('DOMContentLoaded', function() {
 			nextEl: '.swiper-button-next',
 			prevEl: '.swiper-button-prev',
 		}
-    })
+    });
+
+    var mySwiper = new Swiper ('.secondary-slider', {
+		// Optional parameters
+		loop: true,
+		slidesPerView: 1,
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		}
+    });
 
 	var burgerMenu = document.querySelector('.burger-menu'),
 		navigation = document.querySelector('.navigation');
